72f6a38dd7a0fd9524ecc51852d015508e13a187
Praphul Kumar <praphulsingh1712@gmail.com>
2024-04-12 00:52:24 +0530

    work section done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,9 +7,30 @@ import {logo,menu,close} from '../assets'
 const Navbar = () => {
   const [active,setActive]=useState("");
   const [ toggle, setToggle ] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      if (scrollTop > 100) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <nav 
-      className={`${styles.paddingX} w-full flex itmes-center py-5 fixed top-0 z-20`}
+    <nav
+      className={`${
+        styles.paddingX
+      } w-full flex itmes-center py-5 fixed top-0 z-20 ${
+        scrolled ? "bg-primary" : "bg-transparent"
+      }`}
     >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
@@ -23,8 +44,7 @@ const Navbar = () => {
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
             Praphul &nbsp;
-            <span className="sm:block hidden">
-              Kumar</span>
+            <span className="sm:block hidden">Kumar</span>
           </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
@@ -62,7 +82,8 @@ const Navbar = () => {
                   }font-popins font-medium cursor-pointer text-[16px]`}
                   onClick={() => {
                     setToggle(!toggle);
-                    setActive(nav.title)}}
+                    setActive(nav.title);
+                  }}
                 >
                   <a href={`#${nav.id}`}>{nav.title}</a>
                 </li>
